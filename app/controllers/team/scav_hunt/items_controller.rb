1e45ad10016f743d7f806fd1788ccde0bca3fe0a
isaiahzs <isaiahzs@dvtk.me>
2025-04-19 10:26:11 -0500

    Sort list by category; create item with category

--- app/controllers/team/scav_hunt/items_controller.rb
+++ app/controllers/team/scav_hunt/items_controller.rb
@@ -2,19 +2,21 @@ class Team::ScavHunt::ItemsController < Team::ScavHunt::Item::BaseController
   skip_before_action :set_item, only: [:index, :index_mine, :new, :create, :search]
 
   def index
-    @items_by_page = @team_scav_hunt.items.group_by(&:page_number)
+    items = @team_scav_hunt.items.order(:list_category_id, 'page_number ASC NULLS FIRST', :number).left_joins(:list_category)
+    @items_by_category_by_page = items.chunk { |i| i.list_category&.name || :nil }.map { |c, ps| [c, ps.chunk { |p| p.page_number || :nil }] }
   end
 
   def index_mine
-    @items = @team_scav_hunt.items.joins(:item_users).where(item_users: {user_id: @user.id})
+    @items = @team_scav_hunt.items.joins(:item_users).where(item_users: {user_id: @user.id}).order(:number)
   end
 
   def new
+    @list_categories = ListCategory.where(team_id: [nil, @team.id])
   end
 
   def create
-    item = @team_scav_hunt.items.create(params[:item].permit(:number, :page_number, :content))
-    redirect_to action: :show, id: item.id
+    item = @team_scav_hunt.items.create(params[:item].permit(:number, :page_number, :content, :list_category_id))
+    redirect_to team_scav_hunt_item_path(@team_scav_hunt, *item.for_url)
   end
 
   def edit
