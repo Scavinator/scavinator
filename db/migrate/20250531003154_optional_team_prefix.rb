61be1c071b450fc4f731209a5630d1b659359163
isaiahzs <isaiahzs@dvtk.me>
2025-07-21 08:40:44 -0500

    Disable ActiveRecord migrations in favor of scavinator-database

--- db/migrate/20250531003154_optional_team_prefix.rb
+++
@@ -1,6 +0,0 @@
-class OptionalTeamPrefix < ActiveRecord::Migration[8.0]
-  def change
-    change_column_null :teams, :prefix, true
-    change_column_null :team_scav_hunts, :scav_hunt_id, true
-  end
-end
