376aab62b1bf893c17d0581131f03150054dd97f
isaiahzs <isaiahzs@dvtk.me>
2026-04-23 10:46:11 -0500

    Update calendar colors

--- app/helpers/team/scav_hunt/events_helper.rb
+++ app/helpers/team/scav_hunt/events_helper.rb
@@ -1,11 +1,9 @@
 module Team::ScavHunt::EventsHelper
   def urgency_css_class(event)
-    if event.date < 15.minutes.ago
+    if event.date < 10.minutes.ago
       "grey"
-    elsif event.date < 10.minutes.from_now
-      "red"
     elsif event.date < 30.minutes.from_now
-      "yellow"
+      "red"
     else
       "initial"
     end
