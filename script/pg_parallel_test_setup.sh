b5f7ee8de9706849e0d3bc41de128b2b98b3f095
isaiahzs <isaiahzs@dvtk.me>
2026-05-16 20:07:50 -0500

    Clean up test setup script

--- script/pg_parallel_test_setup.sh
+++ script/pg_parallel_test_setup.sh
@@ -6,5 +6,4 @@ FILE=$(mktemp)
 for i in {0..32}; do
     printf "%s\n" "CREATE DATABASE \"scavinator_test_$i\" WITH TEMPLATE scavinator_test;" >> $FILE
 done
-cat $FILE
-PGPASSWORD=password psql -f $FILE
+psql -f $FILE
