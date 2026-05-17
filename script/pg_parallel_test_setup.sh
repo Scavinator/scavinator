8cb95efe80b96d97a536772d7307b5f3a0520584
isaiahzs <67109830+isaiahzs@users.noreply.github.com>
2026-05-16 20:09:35 -0500

    Run CI on GHA (#2)

---
+++ script/pg_parallel_test_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+FILE=$(mktemp)
+for i in {0..32}; do
+    printf "%s\n" "CREATE DATABASE \"scavinator_test_$i\" WITH TEMPLATE scavinator_test;" >> $FILE
+done
+psql -f $FILE
